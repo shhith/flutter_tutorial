c4b7df5b71756c27a59f1cb06be3c8f2bcee8fe7
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-11-29 13:35:57 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,17 +26,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'welcome to Flutter',
       // 複数の部品を束ねる
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Welcome to Flutter!!'),
         ),
         body: Center(
-          // asPascalCaseという属性を指定する
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
